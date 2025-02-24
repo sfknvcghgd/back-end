bf14b736ff4be6dd987f1e0949000d1f617d722f
lucas <lucas.ivachuka@gmail.com>
2025-02-24 12:24:23 -0300

    gffyi

---
+++ bolean.js
@@ -0,0 +1,20 @@
+let estudantes = true;
+let estudiosos = false;
+if(estudantes){
+    console.log("Tem direito a meia entrada !")
+} else {
+    console.log("Pague inteira !")
+}
+/*OPERADORES JAVASCRIPT
+OPERADOR DE IGUALDADE :
+COMPARA APENAS O VALOR */
+console.log(true == 1 );  //TRUE
+console.log(false == 0);  //TRUE
+console.log("1" == 1);  //  TRUE 
+// Operador de igualdade estrita :
+// Compara valor e tipo do dado
+console.log(true === 1 )// false
+console.log(false === 0) // false
+console.log("1" === 1) // false
+
+//varificar tipo da variável 
---
+++ condicao.js
@@ -0,0 +1,14 @@
+// condição 
+/* se (condição ) {
+    código aqui 
+} se não {
+    código aqui }
+*/
+let idade = 18 
+let  = true 
+if (idade >= 18 && possuiCNH){
+    console.log("Você pode dirigir")
+  }else {
+    console.log("Ande de ônibus ou Uber!")
+  }
+
