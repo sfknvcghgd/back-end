c1bd5fd60371a20b0e2ba9a6c1e42c0d0c1391a4
lucas <lucas.ivachuka@gmail.com>
2025-02-24 12:22:21 -0300

    notas

---
+++ ezercicío.js
@@ -0,0 +1,27 @@
+let av1 = 10.0; 
+let av2 = 1.0;
+let recup1 = 7.0;
+let recup2 = 0.0;
+let nota1;
+let nota2;
+
+if (recup1 > av1){
+    nota1 = recup1
+} else {
+    nota1 = av1
+}
+
+
+if (recup2 > av2){
+    nota2 = recup2
+} else {
+    nota2 = av2
+}
+
+let med = (nota1 + nota2)/2
+
+if (med >= 6.0){
+console.log("Você passou com média " + med);
+}else {
+console.log("Você reprovou com média " + med);
+}
