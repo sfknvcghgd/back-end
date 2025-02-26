3beda3ac47c5b8cdf39f1b84cc687a6421b7fb8c
lucas <lucas.ivachuka@gmail.com>
2025-02-26 11:38:47 -0300

    dtyd

--- ezercicío.js
+++ ezercicío.js
@@ -1,5 +1,5 @@
 let av1 = 10.0; 
-let av2 = 1.0;
+let av2 = 6.0;
 let recup1 = 7.0;
 let recup2 = 0.0;
 let nota1;
@@ -11,7 +11,6 @@ if (recup1 > av1){
     nota1 = av1
 }
 
-
 if (recup2 > av2){
     nota2 = recup2
 } else {
@@ -21,7 +20,7 @@ if (recup2 > av2){
 let med = (nota1 + nota2)/2
 
 if (med >= 6.0){
-console.log("Você passou com média " + med);
+    console.log("Você passou com média " + med);
 }else {
-console.log("Você reprovou com média " + med);
+    console.log("Você reprovou com média " + med);
 }
