aa61122357a6ec01a1907ba8aa92964c85cf3c51
lucas <lucas.ivachuka@gmail.com>
2025-03-17 12:02:50 -0300

    RTRS

---
+++ aula2.js
@@ -0,0 +1,24 @@
+let alimentos = ["café", "pão", "pão de mel ", "leite", "nescal", "cachorro quente", "refri"]
+let valores = [10 , 50 , 4 , 0 , 2 , 30 , 10 ]
+contador = 0
+while(contador < alimentos.length){
+    let c2 = contador + 1 
+    console.log(c2 + " - " + alimentos [contador] + " R$ " + valores[contador])
+    contador = contador + 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ media.js
@@ -0,0 +1,13 @@
+tri1 =  [10 , 8]
+tri2 = [6 , 4]
+tri3 = [4 , 8]
+
+defmedia (listaNotas);
+    soma = listaNotas [0] + listaNotas [1]
+    media = soma /2
+    retrummedia
+
+mediatri1 = media (tri1)
+mediatri2 = media (tri2)
+mediatri3 = media (tri3)
+print(mediatri1 , mediatri2 , mediatri3 )
