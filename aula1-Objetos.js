fad471568c092f5261c9147f97f605527aee488a
lucas <lucas.ivachuka@gmail.com>
2025-04-04 11:37:09 -0300

    \H\SDHG

---
+++ aula1-Objetos.js
@@ -0,0 +1,34 @@
+let nomeLanches = ["x-burguer", "x-salada", "x-tudo"];
+
+
+//objetos
+let lanches = [
+    {
+    nome:"x-burguer",
+    preco: 50,
+    ingredientes : ["pão", "queijo", "hambúrguer"]
+    },
+    {
+    nome:"x-salada",
+    preco: 64,
+    ingredientes : ["pão", "queijo", "hambúrguer", "salada"]
+    },
+    {
+    nome:"x-tudo",
+    preco: 320,
+    ingredientes : ["pão", "queijo", "hambúrguer", "salada", "tudo"]
+    }
+]
+console.log("Nome: " + lanches[0].nome + " - preço: " + lanches[0].preco);
+console.log("ingredientes: " + lanches[0].ingredientes);
+
+
+
+
+
+
+
+
+
+
+
